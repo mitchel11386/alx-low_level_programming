cc126e993dbf228d8930ecb2084e9d341e621351
mitchel11386 <mitchelabraham20@gmail.com>
2023-02-20 16:07:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main - entry poin
+ *  
+ * return: Always 0 (success)
+ */
+int main(void)
+{ 
+        puts("\"programing is like building a multilingual puzzle");
+        return 0;
+}

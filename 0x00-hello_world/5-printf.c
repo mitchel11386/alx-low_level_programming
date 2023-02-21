d0b3d2e6d3d35c1df362361c503a5230e12f55c5
mitchel11386 <mitchelabraham20@gmail.com>
2023-02-20 18:39:32 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-       printf("with proper grammar, but the outcome is a piece of art")
-       return (0);
+        printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+        printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+        printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        return (0); 
+	printf("with proper grammar, but the outcome is a piece of art")
 }
